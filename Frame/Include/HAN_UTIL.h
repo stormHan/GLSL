6ddc34f2b0807b4f7026587bfa3fda69e4c1ebe4
JWhan <stormhan1205@gmail.com>
2016-03-16 18:00:31 +0800

    rebuild a glsl frame using very amzaing C++

---
+++ Frame/Include/HAN_UTIL.h
@@ -0,0 +1,32 @@
+/*
+
+
+我自己写的一个图形学通用库，可用于辅助读取shader以及一些其它功能
+
+
+我自己写的一个图形学通用库，可用于辅助读取shader以及一些其它功能
+
+*/
+
+#ifndef H_HAN_UTIL_H
+#define H_HAN_UTIL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+#ifndef M_PI
+#define M_PI 3.14159265f
+#endif
+
+bool ReadFile(const char* filename, string& outFile);
+
+void FileReadingError(const char* pFileName, unsigned int line, const char* pError);
+
+#define FILE_READING_ERROR(FileError) FileReadingError(__FILE__, __LINE__, FileError); 
+
+
+
+#endif
